6407f6c3a8a4be09ecc919a0ca789743d9777c2f
Ayush Tiwari <01tiwari.ayush@gmail.com>
2023-07-14 11:25:46 +0530

    reponsive side-cart now

--- script.js
+++ script.js
@@ -15,10 +15,18 @@ var swiper = new Swiper(".mySwiper", {
 const menuBtn = document.querySelector(".menu-btn");
 const sideMenu = document.querySelector(".responsive-side-menu");
 const sideMenuCrossBtn = document.querySelector(".side-menu-cross-btn");
+const cartBtn = document.querySelector(".cart-btn");
+const sideCart = document.querySelector(".side-cart");
 
 menuBtn.addEventListener("click", () => {
   sideMenu.classList.add("active");
 });
-sideMenu.addEventListener("click", () => {
+sideMenuCrossBtn.addEventListener("click", () => {
   sideMenu.classList.remove("active");
 });
+function openSideCart() {
+  sideCart.classList.add("active");
+}
+function closeSideCart() {
+  sideCart.classList.remove("active");
+}
